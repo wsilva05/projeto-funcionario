1166f72aafca7d89c92ab4f267c0ec929c4fdb23
wsilva05 <wsilva05@gmail.com>
2019-08-17 12:20:57 -0300

    estou salvando testes

---
+++ prj15_revisao_exercicio/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj15_revisao_exercicio
+{
+    class Funcionario
+    {
+        public string Colaborador { get; set; }
+        public string Cargo { get; set; }
+        public decimal Salario { get; set; }
+        public decimal Desconto { get; set; }
+        
+        /*
+        public decimal NovoSalario(decimal desc)
+        {
+            novoSalario = Salario - (desc / 100 * Salario);
+            return novoSalario;
+        }
+        */
+     
+        public override string ToString()
+        {
+            return string.Format("{0} {1} tem um salário bruto de R${2}, com desconto de {3}%, salário será atualizado para R${4}.",
+                Cargo, Colaborador, Salario, Desconto.ToString("f2", CultureInfo.InvariantCulture),
+                Salario - ((decimal)Desconto / 100 * Salario) );
+        }
+        
+    }
+}
---
+++ prj15_revisao_exercicio/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj15_revisao_exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.Write("Informe o nome do funcionário(a): ");
+            func.Colaborador = Console.ReadLine();
+
+            Console.Write("Informe o cargo desse funcionário(a): ");
+            func.Cargo = Console.ReadLine();
+
+            Console.Write("Informe o salário desse funcionário(a): ");
+            func.Salario = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Informe o percentual de desconto: ");
+            func.Desconto = decimal.Parse(Console.ReadLine());
+
+            /*
+            Console.Write("{0} informe o valor de desconto: ", func.Colaborador);
+            int desc = int.Parse(Console.ReadLine());
+            Console.WriteLine(func.NovoSalario(desc));
+            */
+
+            Console.WriteLine();
+            Console.WriteLine(func);
+
+        }
+    }
+}
